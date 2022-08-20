cc52e829288f76240aadded411ac345b0b70279e
John <ebusd@ebusd.eu>
2022-08-20 10:45:26 +0200

    adjust to newer esptool.py, add hint for use in WSL, check flash size before flashing

--- flash.sh
+++ flash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # flash an ESP8266, ESP8285 or ESP32 with the latest firmware with forced erase.
-# requires platformio including tool-esptoolpy installed on the user
-# and for ESP8266 additionally an installed Non-OS SDK
+# requires platformio including tool-esptoolpy (>=3.3) installed on the user account (~/.platformio/packages/tool-esptoolpy)
+# and for ESP8266 additionally an installed Non-OS SDK.
+# when run in WSL, usbipd can be used to forward the USB serial port into WSL with e.g. "usbipd wsl attach --busid 7-2".
 
 fw=${1:-./dist/ebus-v3_d1mini.bin}
 home=${HOMEDIR:-~}
@@ -24,23 +25,50 @@ if [[ "${fw%32_factory.bin}" != "$fw" ]] || [[ "${fw%32.bin}" != "$fw" ]]; then
     eraseall=''
   fi
   chip='esp32'
+  size='4MB'
+  echo "checking flash size"
+  detectsize=`$tooldir/esptool.py --chip=$chip --baud $speed -a no_reset flash_id`
+  if [[ $? -ne 0 ]]; then
+    echo "error checking flash size:" >&2
+    echo "$detectsize" >&2
+    exit 1
+  fi
+  detectsize=`echo "$detectsize"|grep "flash size"|sed -e 's#.*: ##'`
+  if [[ "$detectsize" != "$size" ]]; then
+    echo "flash size mismatch: expected $size, detected $detectsize." >&2
+    exit 1
+  fi
+  echo "flashing $fw with size $size"
   $tooldir/esptool.py --chip=$chip --baud $speed --after hard_reset \
-    write_flash $eraseall --compress \
+    write_flash $eraseall --flash_size $size --compress \
     0 "$fw"
 else
   # ESP8266 or ESP8285 firmware
   imgdir=$home/.platformio/packages/framework-esp8266-nonos-sdk/bin
   chip='esp8266'
-  size='4m'
+  size='4MB'
   last='0x3f'
   if [[ "${fw%_16m.bin}" != "$fw" ]]; then
-    size='16m'
+    size='16MB'
     last='0xff'
   elif [[ "${fw%_1m.bin}" != "$fw" ]]; then
-    size='1m'
+    size='1MB'
     last='0xf'
     speed=115200
   fi
+  echo "checking flash size"
+  detectsize=`$tooldir/esptool.py --chip=$chip --baud $speed -a no_reset flash_id`
+  if [[ $? -ne 0 ]]; then
+    echo "error checking flash size:" >&2
+    echo "$detectsize" >&2
+    exit 1
+  fi
+  detectsize=`echo "$detectsize"|grep "flash size"|sed -e 's#.*: ##'`
+  if [[ "$detectsize" != "$size" ]]; then
+    echo "flash size mismatch: expected $size, detected $detectsize." >&2
+    exit 1
+  fi
+  echo "flashing $fw with size $size"
   $tooldir/esptool.py --chip=$chip --baud $speed --after hard_reset \
     write_flash $eraseall --flash_size $size --compress \
     "${last}b000" "$imgdir/blank.bin" \
